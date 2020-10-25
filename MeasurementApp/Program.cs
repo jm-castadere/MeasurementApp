a66e3597c86c1ecbaf5c4ec720ff92559e428803
Jean-Michel Castadere <af359@msoffice.group>
2020-10-25 19:27:05 +0100

    code

--- MeasurementApp/Program.cs
+++ MeasurementApp/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using MeasurementApp.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.Linq;
-using MeasurementApp.Models;
 
 namespace MeasurementApp
 {
@@ -10,9 +9,14 @@ namespace MeasurementApp
     {
         static void Main(string[] args)
         {
-            //Start mapping
+            foreach (Measurement itemMeasure in FilListOfMeasures())
+            {
+                Console.WriteLine($"All Measuren-> Time :{itemMeasure.measurementTime} Type :{itemMeasure.measurementType} Value :{itemMeasure.measurementValue}");
+            }
 
-            ////for each Measurement record
+            Console.WriteLine("----------------------------------");           
+            
+            //Start mapping and write to console
             foreach (Measurement itemMeasure in MapMeasure(FilListOfMeasures(), new DateTime(2017, 1, 3, 10, 0, 0), 5))
             {
                 Console.WriteLine($"Time :{itemMeasure.measurementTime} Type :{itemMeasure.measurementType} Value :{itemMeasure.measurementValue}");
@@ -22,11 +26,11 @@ namespace MeasurementApp
         }
 
         /// <summary>
-        /// Map all measuren record and return measuren-list found   
+        /// Map all measure record and return measure-list found   
         /// </summary>
         /// <param name="measureList">Measure list to map</param>
         /// <param name="startDateGrid">date of the grid or begins the mapping </param>
-        /// <param name="dateIntervalMinute">measured values interval in minute (startdate+ intervalle) </param>
+        /// <param name="dateIntervalMinute">measured values interval in minute (startdate and startdate+intervalle) </param>
         public static List<Measurement> MapMeasure(List<Measurement> measureList, DateTime startDateGrid, int dateIntervalMinute)
         {
             List<Measurement> measureReturned = new List<Measurement>();
@@ -39,53 +43,51 @@ namespace MeasurementApp
             {
                 do
                 {
-                    //Retriev all Measuren of mesure type and in time interval
+                    //Retriev all Measuren of mesure-type 
                     var measureAllValueOfTyp = ExtractMeasureTyp(measureList, itemTyp);
 
-                    //retrieve last measure record between the dates Grid From and To
+                    //retrieve last measure record between the dateGrid-From and dateGrid-To 
                     var measureFund = measureAllValueOfTyp.LastOrDefault(a => a.measurementTime > startDateGrid &&
-                                                                              a.measurementTime <=
-                                                                              startDateGrid.AddMinutes(
-                                                                                  dateIntervalMinute));
+                                                           a.measurementTime <= startDateGrid.AddMinutes(dateIntervalMinute));
                     if (measureFund != null)
                     {
+                        //set Measure and add to measuren-list
                         Measurement recordFund = new Measurement
                         {
                             measurementTime = startDateGrid.AddMinutes(dateIntervalMinute),
                             measurementValue = measureFund.measurementValue,
                             measurementType = measureFund.measurementType
                         };
-
                         measureReturned.Add(recordFund);
 
+                        //set next dategrid-from 
                         startDateGrid = startDateGrid.AddMinutes(dateIntervalMinute);
 
+                        //Set flag continue mapping for next measure-type
                         isLoop = true;
                     }
                     else
                     {
+                        //Reset start datefrid for next Measure Type
                         startDateGrid = dateGridReset;
+                       
+                        //Set flag to stopt mapping
                         isLoop = false;
                     }
                 } while (isLoop);
                 
 
             }
-
-
+            
             return measureReturned;
-
-          
 
         }
 
 
-
-
         /// <summary>
-        /// Extract and return the measuren-value list of the selected measure-type
+        /// Extract and return the measure-value list of the selected measure-type
         /// </summary>
-        /// <param name="measureList">Measuren-list</param>
+        /// <param name="measureList">Measure-list</param>
         /// <param name="measureType">Measure-type to select</param>
         /// <returns>Measure list of Type selected</returns>
         private static IOrderedEnumerable<Measurement> ExtractMeasureTyp(List<Measurement> measureList, MeasureType measureType)
@@ -103,9 +105,9 @@ namespace MeasurementApp
 
 
         /// <summary>
-        /// Extract and return Measure-Typ list contained in Measuren-list
+        /// Extract and return Measure-Typ list contained in Measure-list
         /// </summary>
-        /// <param name="measureList">Measuren-list</param>
+        /// <param name="measureList">Measure-list</param>
         /// <returns>single Measure-typ list</returns>
         private static List<MeasureType> ExtractListMeasureTyp(List<Measurement> measureList)
         {
@@ -126,33 +128,15 @@ namespace MeasurementApp
             return null;
 
         }
+        
 
-
-
-
+        
 
 
         /// <summary>
-        /// Creates and returns a list of sample measures 
+        /// Filled sample measure
         /// </summary>
-        /// <returns>Measurement list</returns>
-        private static Measurement[] FilledListOfMeasures()
-        {
-            Measurement[] lisMeasurement = {
-                new Measurement(){ measurementTime= new DateTime(2017, 1, 3,10,4,45), measurementValue= 35.79,measurementType = MeasureType.TEMP},
-                new Measurement(){ measurementTime= new DateTime(2017, 1, 3,10,1,18), measurementValue= 98.78,measurementType = MeasureType.SPO2},
-                new Measurement(){ measurementTime= new DateTime(2017, 1, 3,10,9,7), measurementValue= 35.01,measurementType = MeasureType.TEMP},
-                new Measurement(){ measurementTime= new DateTime(2017, 1, 3,10,3,34), measurementValue= 96.49,measurementType = MeasureType.SPO2},
-                new Measurement(){ measurementTime= new DateTime(2017, 1, 3,10,2,1), measurementValue= 35.82,measurementType = MeasureType.TEMP},
-                new Measurement(){ measurementTime= new DateTime(2017, 1, 3,10,5,0), measurementValue= 97.17,measurementType = MeasureType.SPO2},
-                new Measurement(){ measurementTime= new DateTime(2017, 1, 3,10,5,1), measurementValue= 95.08,measurementType = MeasureType.SPO2},
-            };
-
-            return lisMeasurement;
-
-        }
-
-
+        /// <returns></returns>
         private static List<Measurement> FilListOfMeasures()
         {
             List<Measurement> listReturned = new List<Measurement>
@@ -175,7 +159,7 @@ namespace MeasurementApp
                     measurementValue = 35.01,
                     measurementType = MeasureType.TEMP
                 },
-                new Measurement()
+              new Measurement()
                 {
                     measurementTime = new DateTime(2017, 1, 3, 10, 3, 34),
                     measurementValue = 96.49,
